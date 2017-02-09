c7af87677125d862f2cacba986c06cd10a6923a1
Daniel Markiewicz <daniel.markiewicz92@gmail.com>
2017-02-09 11:32:57 -0500

    Doesn't actually work.

--- app/src/main/java/tk/strictlyconformist/medusa/MainActivity.java
+++ app/src/main/java/tk/strictlyconformist/medusa/MainActivity.java
@@ -3,11 +3,27 @@ package tk.strictlyconformist.medusa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static Socket connect(String hostname, int port) throws IOException {
+        Socket  targetSocket = new Socket(hostname, port);
+        return targetSocket;
+    }
+
+    public static void logIn(String username, String password, Socket commandSocket) throws IOException{
+        PrintWriter out =
+                new PrintWriter(commandSocket.getOutputStream(),true);
+        out.print("USER"+username+"\r\n");
+        out.print("PASS"+password+"\r\n");
     }
 }
